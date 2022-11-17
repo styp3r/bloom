a35c2d018833dcfec325b40a1f44f466e616c73e
styp3r <stypertypes.com>
2022-11-17 12:05:40 +0530

    Removes header and adds websiteName to sidebar and makes sidebar stick to the left at all times even during scroll

--- src/components/CategoriesSidebar.js
+++ src/components/CategoriesSidebar.js
@@ -1,6 +1,7 @@
 export default function CategoriesSidebar() {
     return (
         <div id = "categoriesSidebar">
+            <div id="websiteName">fublot.</div>
             <div id="categories">
                 {/*Add Link tags from react-router-dom for each category*/}
             </div>
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-export default function Header(){
-
-    return (
-        <div id="header">
-            <div id="websiteName">fublot.</div>
-            <input id="searchbar" placeholder="Search fublot"/>
-        </div>
-    )
-}
