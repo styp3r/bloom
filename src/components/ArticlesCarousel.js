1010533859348557daa2fc59946837b740219465
styp3r <stypertypes.com>
2022-11-17 11:26:02 +0530

    Creates articles carousel section

--- src/components/ArticlesCarousel.js
+++ src/components/ArticlesCarousel.js
@@ -1,5 +1,19 @@
-export default function ArticlesCarousel(){
+export default function ArticlesCarousel() {
     return (
-        <div id="carouselContainer"></div>
+        <div id="carouselContainer">
+            <div className="main-carousel-container">
+                <div className="main-carousel"></div>
+            </div>
+            <div className="sub-carousels">
+                <div className="sub-carousel-1">
+                    <div className="sub-carousel-content1"></div>
+                    <div className="sub-carousel-content2"></div>
+                </div>
+                <div className="sub-carousel-2">
+                    <div className="sub-carousel-content3"></div>
+                    <div className="sub-carousel-content4"></div>
+                </div>
+            </div>
+        </div>
     );
 }
